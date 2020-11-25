dfce62de52519d4a933c4ec35012c2dd608d5241
mahirusan <mahirusan564@gmail.com>
2020-11-25 14:49:13 +0000

    モデル作成

--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+
+#メインタスク「メインストーリー前編」などのタイトルが表示される
+class MainTask(models.Model):
+    title = models.CharField('タイトル',max_length=64)
+    TYPES = (
+        (1,'メインストーリー'),
+        (2,'外伝'),
+        (3,'オリジナルタスク'),
+        (4,'その他'),
+    )
+    types = models.IntegerField('タイプ',choices=TYPES)
+
+    def __str__(self):
+        return self.title
+
+
+#サブタスク「第64話」みたいなメインに対する細かいタスクが入る
+class SubTask(models.Model):
+    main_task = models.ForeignKey(MainTask,on_delete=models.CASCADE)
+    title = models.CharField('タイトル',max_length=128)
+    content = models.TextField('詳細')
+    CONDITIONS = (
+        (1,'新規'),
+        (2,'挑戦中'),
+        (3,'完了'),
+    )
+    condition = models.IntegerField('状態',choices=CONDITIONS,default=1)
+    created_at = models.DateTimeField('作成日',auto_now=True)
+    updated_at = models.DateTimeField('更新日',auto_now=True)
+    
+
