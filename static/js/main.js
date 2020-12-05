04b0556decc4bddae8aba2c77444f446b269149f
mahirusan <mahirusan564@gmail.com>
2020-12-05 16:09:12 +0000

    js修正(タスク移動不具合)

--- static/js/main.js
+++ static/js/main.js
@@ -6,24 +6,25 @@ document.addEventListener('DOMContentLoaded',function(){
     // $('.fin-tasks').toggle();
 });
 function sortCard(cards){
-        var sortedCards = cards.sort(function(a,b){
-            // console.log($(a).data('num'));
-            // a,bのカスタムデータを取得
-            var aNum = $(a).data('num');
-            var bNum = $(b).data('num');
+    var sortedCards = cards.sort(function(a,b){
+        // console.log($(a).data('num'));
+        // a,bのカスタムデータを取得
+        var aNum = $(a).data('num');
+        var bNum = $(b).data('num');
 
-            // 昇順ソート(不等号を逆にすれば降順にもできる)
-            if(Number(aNum) > Number(bNum)){
-                return 1;
-            }else if(Number(aNum) < Number(bNum)){
-                return -1;
-            }
-            return 0;
-        });
-        // console.log(sortedCard);
-        return sortedCards;
-    }
+        // 昇順ソート(不等号を逆にすれば降順にもできる)
+        if(Number(aNum) > Number(bNum)){
+            return 1;
+        }else if(Number(aNum) < Number(bNum)){
+            return -1;
+        }
+        return 0;
+    });
+    // console.log(sortedCard);
+    return sortedCards;
+}
 
+//ドキュメント全体のsuccess_btnクラスにクリックイベントを追加
 $(document).on('click','.success_btn',function(){
     var $this = $(this);
     console.log($this[0]);
@@ -38,19 +39,19 @@ $(document).on('click','.success_btn',function(){
         var finTasks = card.parents('.card-body').children('.fin-tasks');
         // 状態変化+完了タスクリストに入れる
         $this[0].dataset.condition = "3";
-        card.parent().fadeOut(150,function(){
+        card.parent().fadeOut('fast').queue(function(){
             $(this).appendTo(finTasks);
             $(this).show();
-        });
-        // card.parent().appendTo(finTasks);
 
-        // 並び替え
-        var sortedFinTasks = sortCard(finTasks.children());
-        finTasks.empty();
-        sortedFinTasks.each(function(){
-            finTasks.append($(this));
-        });
-        console.log("完了");
+            //以下の処理をfadeOut外に持っていくとfinTasksに
+            //要素が移動しきらず次の処理に行くので中に入れました
+            var sortedFinTasks = sortCard(finTasks.children()); // 並び替え
+            finTasks.empty();
+            sortedFinTasks.each(function(){
+                finTasks.append($(this));
+            });
+            console.log("完了");
+        }); 
     }else if(condition == "3"){
         var newTasks = card.parents('.card-body').children('.new-tasks');
         // 状態変化+新規タスクリストに入れる
@@ -58,15 +59,15 @@ $(document).on('click','.success_btn',function(){
         card.parent().fadeOut(150,function(){
             $(this).appendTo(newTasks);
             $(this).show();
-        });
-        // card.parent().appendTo(newTasks);
 
-        // 並び替え
-        var sortedNewTasks = sortCard(newTasks.children());
-        newTasks.empty();
-        sortedNewTasks.each(function(){
-            newTasks.append($(this));
+            //以下の処理をfadeOut外に持っていくとfinTasksに
+            //要素が移動しきらず次の処理に行くので中に入れました
+            var sortedNewTasks = sortCard(newTasks.children()); // 並び替え
+            newTasks.empty();
+            sortedNewTasks.each(function(){
+                newTasks.append($(this));
+            });
+            console.log("完了");
         });
-        console.log("完了");
     }
 });
